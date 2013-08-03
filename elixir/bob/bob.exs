fce395c16934a8abb19830de2f0102377c91fb6a
Bob Ippolito <bob@redivi.com>
2013-08-03 09:46:02 -0700

    nit fix

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,12 +1,17 @@
 defmodule Teenager do
+  @doc "Bob's response to the given fragment of conversation"
   def hey(s) do
     cond do
+      # Nothing was said
       s === "" ->
         "Fine. Be that way!"
+      # RESPONSE TO YELLING!
       s === String.upcase(s) ->
         "Woah, chill out!"
+      # A question?
       String.ends_with?(s, "?") ->
         "Sure."
+      # Default response
       true ->
         "Whatever."
     end 
