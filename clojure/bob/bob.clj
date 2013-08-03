a080d1b2a96420ae440c0b392923b0921657a122
Bob Ippolito <bob@redivi.com>
2013-08-03 09:41:55 -0700

    address nit

--- clojure/bob/bob.clj
+++ clojure/bob/bob.clj
@@ -1,8 +1,13 @@
 (ns bob)
 (defn response-for
+  "Bob's response to the given fragment of conversation"
   [s]
   (cond
+   ;; Nothing was said 
    (empty? s) "Fine, be that way."
+   ;; YELLING IN ALL CAPS.
    (= s (.toUpperCase s)) "Woah, chill out!"
+   ;; A question?
    (= \? (last s)) "Sure."
+   ;; Default response
    :else "Whatever."))
