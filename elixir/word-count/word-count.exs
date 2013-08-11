4f401d7e2f5b5c69033a9c78500f88d7ca084be5
Bob Ippolito <bob@redivi.com>
2013-08-11 12:26:48 -0700

    use pipeline operator

--- elixir/word-count/word-count.exs
+++ elixir/word-count/word-count.exs
@@ -1,7 +1,9 @@
 # Note that the & special form requires Elixir 0.10.1.
 defmodule Words do
   def count(s) do
-    List.foldl(split_words(normalize(s)), HashDict.new([]), &inc_word/2)
+    s |> String.downcase
+      |> String.split(%r/\W+/)
+      |> List.foldl(HashDict.new([]), &inc_word/2)
   end
 
   defp inc_word("", dict) do
@@ -9,13 +11,5 @@ defmodule Words do
   end
   defp inc_word(word, dict) do
     HashDict.update(dict, word, 1, &(&1 + 1))
-  end
-
-  defp normalize(s) do
-    String.downcase(s)
-  end
-
-  defp split_words(s) do
-    String.split(s, %r/\W+/)
   end
 end
