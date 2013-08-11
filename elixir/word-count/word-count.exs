98f5f373bcc928d06a6f63c44779de4a6c862f49
Bob Ippolito <bob@redivi.com>
2013-08-11 10:58:35 -0700

    pick nits

--- elixir/word-count/word-count.exs
+++ elixir/word-count/word-count.exs
@@ -1,23 +1,21 @@
 # Note that the & special form requires Elixir 0.10.1.
 defmodule Words do
-  # I find using &inc_word/2 less readable than using a fn inside count/1
-  # Including it here for the nitpicker.
-  #defp inc_word("", dict) do
-  #  dict
-  #end
-  #defp inc_word(word, dict) do
-  #  HashDict.update(dict, word, 1, &(&1 + 1))
-  #end
   def count(s) do
-    inc_word = fn
-      ("", dict) ->
-        dict
-      (word, dict) ->
-        HashDict.update(dict, word, 1, &(&1 + 1))
-    end
-    List.foldl(
-      String.split(String.downcase(s), %r/\W+/),
-      HashDict.new([]),
-      inc_word)
+    List.foldl(split_words(normalize(s)), HashDict.new([]), &inc_word/2)
+  end
+
+  defp inc_word("", dict) do
+    dict
+  end
+  defp inc_word(word, dict) do
+    HashDict.update(dict, word, 1, &(&1 + 1))
+  end
+
+  defp normalize(s) do
+    String.downcase(s)
+  end
+
+  defp split_words(s) do
+    String.split(s, %r/\W+/)
   end
 end
