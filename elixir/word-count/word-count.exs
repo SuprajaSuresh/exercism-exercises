a28cf64027fe66a0f352b1bfed28090ba4337c41
Bob Ippolito <bob@redivi.com>
2013-08-11 10:03:06 -0700

    use elixir 0.10.1 & special form

--- elixir/word-count/word-count.exs
+++ elixir/word-count/word-count.exs
@@ -1,9 +1,19 @@
+# Note that the & special form requires Elixir 0.10.1.
 defmodule Words do
+  # I find using &inc_word/2 less readable than using a fn inside count/1
+  # Including it here for the nitpicker.
+  #defp inc_word("", dict) do
+  #  dict
+  #end
+  #defp inc_word(word, dict) do
+  #  HashDict.update(dict, word, 1, &(&1 + 1))
+  #end
   def count(s) do
     inc_word = fn
-      ("", dict) -> dict
+      ("", dict) ->
+        dict
       (word, dict) ->
-        HashDict.update(dict, word, 1, fn (v) -> 1 + v end)
+        HashDict.update(dict, word, 1, &(&1 + 1))
     end
     List.foldl(
       String.split(String.downcase(s), %r/\W+/),
