9b2f0461404cfbfb1a943dee7f37bb853d109678
Bob Ippolito <bob@redivi.com>
2013-08-11 22:19:45 -0700

    use frequencies

--- clojure/word-count/word_count.clj
+++ clojure/word-count/word_count.clj
@@ -1,14 +1,12 @@
 (ns phrase
   (:use [clojure.string :only [lower-case split blank?]]))
-(defn- normalize [s] (lower-case s))
+
 (defn- words [s] (remove blank? (split s #"\W+")))
-(defn- word-to-count-map [word] {word 1})
 
 (defn word-count [s]
   "Count the occurrences of each word (one or more contiguous case normalized
   characters that are not punctuation or whitespace) in a string"
   (->> s
-       normalize
+       lower-case
        words
-       (map word-to-count-map)
-       (apply merge-with +)))
+       frequencies))
