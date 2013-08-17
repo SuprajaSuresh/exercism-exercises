23a4bfb7fef376143022603f71db4a1e1df5e7b0
Bob Ippolito <bob@redivi.com>
2013-08-17 09:59:15 -0700

    clj dna

---
+++ clojure/nucleotide-count/dna.clj
@@ -0,0 +1,27 @@
+(ns dna
+  (:refer-clojure :exclude [count]))
+(def ^{:private :const} nucleotides
+  (apply hash-set (map str "ACGTU")))
+(def ^{:private :const} dna-nucleotides
+  (apply hash-set (map str "ACGT")))
+(def ^{:private :const} dna-nucleotide-map
+  (apply hash-map (interleave dna-nucleotides (repeat 0))))
+(defn- normalize-nucleotide
+  [valid-nucleotides input]
+  (if (nil? (valid-nucleotides input))
+    (throw (Exception. (str "invalid nucleotide '" input "'")))
+    input))
+(defn- nucleotides-from-str [valid-nucleotides input]
+  (map (partial normalize-nucleotide valid-nucleotides) (re-seq #"." input)))
+(defn nucleotide-counts
+  "DNA nucleotide counts (A, C, G, T) for the given strand as {string integer}"
+  [strand]
+  (merge dna-nucleotide-map
+         (frequencies (nucleotides-from-str dna-nucleotides strand))))
+(defn count
+  "Return the number of times nucleotide occurs in strand"
+  [nucleotide strand]
+  (let [valid-nucleotide (normalize-nucleotide nucleotides nucleotide)]
+    (->> (nucleotides-from-str nucleotides strand)
+         (filter #(= valid-nucleotide %))
+         (clojure.core/count))))
