ad81bf964cc111043c746b62e675c4635d6d4ade
Bob Ippolito <bob@redivi.com>
2013-08-18 15:37:33 -0700

    hash literals and whitespace

--- clojure/nucleotide-count/dna.clj
+++ clojure/nucleotide-count/dna.clj
@@ -1,23 +1,30 @@
 (ns dna
   (:refer-clojure :exclude [count]))
-(def ^{:private :const} nucleotides
-  (apply hash-set (map str "ACGTU")))
-(def ^{:private :const} dna-nucleotides
-  (apply hash-set (map str "ACGT")))
+
+(def ^:private nucleotides
+  #{"A" "C" "G" "T" "U"})
+
+(def ^:private dna-nucleotides
+  #{"A" "C" "G" "T"})
+
 (def ^{:private :const} dna-nucleotide-map
   (apply hash-map (interleave dna-nucleotides (repeat 0))))
+
 (defn- normalize-nucleotide
   [valid-nucleotides input]
   (if (nil? (valid-nucleotides input))
     (throw (Exception. (str "invalid nucleotide '" input "'")))
     input))
+
 (defn- nucleotides-from-str [valid-nucleotides input]
   (map (partial normalize-nucleotide valid-nucleotides) (re-seq #"." input)))
+
 (defn nucleotide-counts
   "DNA nucleotide counts (A, C, G, T) for the given strand as {string integer}"
   [strand]
   (merge dna-nucleotide-map
          (frequencies (nucleotides-from-str dna-nucleotides strand))))
+
 (defn count
   "Return the number of times nucleotide occurs in strand"
   [nucleotide strand]
