8b69aad8dfd766146d0e15e16abd4bcbbb05f3b5
Bob Ippolito <bob@redivi.com>
2013-08-31 03:07:08 -0700

    checkpoint

--- elixir/point-mutations/point_mutations.exs
+++ elixir/point-mutations/point_mutations.exs
@@ -1,6 +1,5 @@
 defmodule DNA do
   def hamming_distance(as, bs) do
-    List.zip([as, bs]) |>
-    List.foldl(0, fn ({a, b}, acc) -> acc + if a == b do 0 else 1 end end)
+    Enum.count(List.zip([as, bs]), fn ({a, b}) -> a != b end)
   end
 end
