f3a5198b65ac4f8ac1c256b8358a464fe3b546c9
Bob Ippolito <bob@redivi.com>
2013-11-25 15:24:23 -0600

    use count

--- scala/hamming/src/main/scala/Hamming.scala
+++ scala/hamming/src/main/scala/Hamming.scala
@@ -1,6 +1,4 @@
 package object Hamming {
   def compute(a: String, b: String): Int =
-    (0 until a.length.min(b.length)).foldLeft(0)(
-      (acc, i) => if (a(i) != b(i)) 1 + acc else acc
-    )
+    (0 until a.length.min(b.length)).count(i => a(i) != b(i))
 }
