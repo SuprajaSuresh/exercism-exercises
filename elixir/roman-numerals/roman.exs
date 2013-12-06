a32bbfb46ee49d34e079f42154fba68cec7841da
Bob Ippolito <bob@redivi.com>
2013-12-06 10:24:21 -0800

    roman numerals

--- elixir/roman-numerals/roman.exs
+++ elixir/roman-numerals/roman.exs
@@ -2,8 +2,18 @@ defmodule Roman do
   @doc """
   Convert the number to a roman number.
   """
+  @places [ {1000, "M"}, {900, "CM"},
+            { 500, "D"}, {400, "CD"},
+            { 100, "C"}, { 90, "XC"},
+            {  50, "L"}, { 40, "XL"},
+            {  10, "X"}, {  9, "IX"},
+            {   5, "V"}, {  4, "IV"},
+            {   1, "I"} ]
   @spec numerals(pos_integer) :: String.t
   def numerals(number) do
-  
+    Enum.reduce(@places, {number, ""}, fn({n, digit}, {num, res}) ->
+         {rem(num, n), res <> String.duplicate(digit, div(num, n))}
+      end)
+      |> elem(1)
   end
 end
