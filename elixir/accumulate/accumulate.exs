1c1a269a8c7c23bfdcccc2073d7c1699c1503866
Bob Ippolito <bob@redivi.com>
2013-12-08 14:46:15 -0800

    elixir accumulate

--- elixir/accumulate/accumulate.exs
+++ elixir/accumulate/accumulate.exs
@@ -8,7 +8,9 @@ defmodule Accumulate do
   
   @spec accumulate(Enum.t, (any -> as_boolean(term))) :: list
   def accumulate(collection, function) do
-  
+    collection
+    |> Enum.reduce([], fn (x, acc) -> [function.(x) | acc] end)
+    |> Enum.reduce([], fn (x, acc) -> [x | acc] end)
   end
 
 end
