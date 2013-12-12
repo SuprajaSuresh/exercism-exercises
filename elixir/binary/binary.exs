b1307720ad2e98fad482a4570a8a60104fd4c980
Bob Ippolito <bob@redivi.com>
2013-12-11 21:03:54 -0800

    v2

--- elixir/binary/binary.exs
+++ elixir/binary/binary.exs
@@ -7,19 +7,13 @@ defmodule Binary do
   """
   @spec to_decimal(String.t) :: non_neg_integer
   def to_decimal(string) do
-    try do
-      Enum.reduce(
-        String.codepoints(string),
-        0,
-        fn (x, acc) ->
-          (acc <<< 1) ||| cond do
-            x == "0" -> 0
-            x == "1" -> 1
-            true     -> throw(:invalid_digit)
-          end
-        end)
-    catch
-      :invalid_digit -> 0
-    end
+    fold_bit(String.graphemes(string), 0)
   end
+
+  defp fold_bit([d | ds], acc) when d == "0" or d == "1" do
+    fold_bit(ds, (acc <<< 1) ||| if d == "1" do 1 else 0 end)
+  end
+  defp fold_bit([], acc), do: acc
+  defp fold_bit(_, _), do: 0
+
 end
